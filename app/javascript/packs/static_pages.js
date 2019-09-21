b87ed81b312ff10bd999600f2a85b0cb3fb13111
Amin Shah Gilani <amin@gilani.me>
2019-09-21 13:24:19 +0500

    Add SVG WorkLark logo

--- app/javascript/packs/static_pages.js
+++ app/javascript/packs/static_pages.js
@@ -15,6 +15,10 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+const images = require.context('../../assets/images', true)
+const imagePath = (name) => images(name, true)
+
+
 import feather from 'feather-icons/dist/feather.min.js';
 
 const linkKitImages = require.context('../../../vendor/Landkit-1.1.0/src/assets/img', true)
