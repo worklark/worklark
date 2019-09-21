f3fe19324bd276c5f441b8d81d5be46e6c81ee76
Amin Shah Gilani <amin@gilani.me>
2019-09-22 02:05:00 +0500

    Add routes to static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/terms_of_service'
-  get 'static_pages/privacy_policy'
-  get 'static_pages/contact'
-  get 'static_pages/pricing'
+  root to: 'static_pages#home'
+  get 'about' => 'static_pages#about', as: :about
+  get 'terms-of-service' => 'static_pages#terms_of_service', as: :terms_of_service
+  get 'privacy-policy' => 'static_pages#privacy_policy', as: :privacy_policy
+  get 'contact' => 'static_pages#contact', as: :contact
+  get 'pricing' => 'static_pages#pricing', as: :pricing
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
