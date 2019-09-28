8b22b6e2568da7d69a57761df5c5f4005d46730a
Amin Shah Gilani <amin@gilani.me>
2019-09-29 02:41:58 +0500

    Add flash messages to app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,17 @@
 # Common helper configuration
 module ApplicationHelper
+  FLASH_TO_BOOTSTRAP_MAP = { success: :success,
+                             error: :danger,
+                             alert: :warning,
+                             notice: :info }.freeze
+
+  # filters out flash not mapped to bootstrap classes
+  def flash_filter(flash)
+    flash.select{|k, v| FLASH_TO_BOOTSTRAP_MAP.keys.include? k.to_sym}
+  end
+
+  # the
+  def bootstrap_class_for(flash_type)
+    FLASH_TO_BOOTSTRAP_MAP[flash_type.to_sym].to_s
+  end
 end
