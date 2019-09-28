bd8736e519d0d59e3260a8205b400ded2e853cad
Amin Shah Gilani <amin@gilani.me>
2019-09-29 02:41:41 +0500

    Disallow uninvited users from signing up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :timeoutable, :trackable
+
+  validate :invited_approved?
+
+  def invited_approved?
+    invitation = Invitation.find_by(email: email)
+    invited = invitation.present? && invitation.approved?
+    message = "Sorry, you need to be invited to sign up."
+    errors.add(:invitation, message) unless invited
+  end
 end
