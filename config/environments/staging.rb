f91ce8ff08f60fb9fc7426166b06c5cdb1ef9b5a
Amin Shah Gilani <amin@gilani.me>
2019-09-30 05:59:51 +0500

    Fix staging basic auth

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -6,7 +6,7 @@ require Rails.root.join('config/environments/production')
 Rails.application.configure do
   # Restrict access with HTTP Basic Auth for staging environments
   unless ENV['STAGING_AUTH'].blank?
-      config.middleware.use '::Rack::Auth::Basic' do |username, password|
+      config.middleware.use Rack::Auth::Basic do |username, password|
           ENV['STAGING_AUTH'].split(';').any? do |pair|
               [username, password] == pair.split(':')
           end
