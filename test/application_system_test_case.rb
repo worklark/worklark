1fbb706e5c59f5627c012968cc2e5b4d7bcf3da1
Amin Shah Gilani <amin@gilani.me>
2019-10-18 15:28:16 +0500

    Refactor and update tests and fixtures

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,7 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  # by default it was driven using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :headless_chrome
+  parallelize_me!
 end
--- test/system/onboardings_test.rb
+++ test/system/onboardings_test.rb
@@ -3,24 +3,22 @@ require "application_system_test_case"
 class OnboardingsTest < ApplicationSystemTestCase
   # This test is being written like this on 18 Oct 2018, please fix this
   # in the future by:
-  # 1. parallelizing the tests
-  # 2. fetching fixtures directly
-  driven_by :selenium, using: :headless_chrome
-  parallelize_me!
+  # 1. fetching fixtures directly
+
 
   test "first log in should redirect to onboarding" do
-    login
+    login_with_fresh_account
     assert_selector "h1", text: "User Profile"
   end
 
   test "redirect to organization profile after filling user profile" do
-    login
+    login_with_fresh_account
     fill_in_user_profile
     assert_selector "h1", text: "Organization Profile"
   end
 
   test "redirect to dashboard after filling organization" do
-    login
+    login_with_fresh_account
     fill_in_user_profile
     fill_in_organization_profile
     assert_selector "h1", text: "Jobs"
@@ -28,10 +26,10 @@ class OnboardingsTest < ApplicationSystemTestCase
 
   private
 
-  def login
+  def login_with_fresh_account
     visit new_user_session_url
-    # From the :two fixture in users.yml
-    fill_in "Email",          with: "bob@example.org"
+    # From the :fresh fixture in users.yml
+    fill_in "Email",    with: "alice@example.org"
     fill_in "Password", with: "123greetings"
     click_on "Log in", class: 'btn'
   end
