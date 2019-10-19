2645939051881937ea57ee78950da0ede23bf1d4
Amin Shah Gilani <amin@gilani.me>
2019-10-19 17:38:14 +0500

    Fix rubocop bypass bug

--- scripts/pre-push.bash
+++ scripts/pre-push.bash
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Running pre-push hook"
-./scripts/run-rubocop.bash
-./scripts/run-tests.bash
+./scripts/run-rubocop.bash && ./scripts/run-tests.bash
 
 # $? stores exit value of the last command
 if [ $? -ne 0 ]; then
