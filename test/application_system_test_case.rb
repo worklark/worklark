b3f0022697a2eece46cfb27123e70f432f3061f8
Amin Shah Gilani <amin@gilani.me>
2019-10-19 18:22:06 +0500

    Fix headless chrome testing

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,6 +2,9 @@ require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   # by default it was driven using: :chrome, screen_size: [1400, 1400]
-  driven_by :selenium, using: :headless_chrome
+  # driven_by :selenium, using: :headless_chrome
+  driven_by :selenium, using: :chrome,
+                       options: { args: %w[headless disable-gpu no-sandbox
+                                           disable-dev-shm-usage] }
   parallelize_me!
 end
